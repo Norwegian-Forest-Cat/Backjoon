4459c34b6cd63ea51aab8353a067242737a65cb4
이성조 <lsj2837@naver.com>
2023-02-26 23:59:48 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,7 +1,3 @@
 def solution(num_list):
-    N = len(num_list) // 2
-    
-    for i in range(N):
-        num_list[i], num_list[-1-i] = num_list[-1-i], num_list[i]
-        
+    num_list.reverse()
     return num_list
